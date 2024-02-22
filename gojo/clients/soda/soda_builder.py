2fb017a7cf674c6884039550855eeb7bd5c6fed7
cefranllyperez@gmail.com <cefra.perez@conversenow.ai>
2024-02-22 12:14:12 -0300

    Cleaned soda client

--- gojo/clients/soda/soda_builder.py
+++ gojo/clients/soda/soda_builder.py
@@ -23,12 +23,26 @@ class SodaBuilder:
    def __init__(self, soda_scan: Scan) -> None:
       self._soda_scan = soda_scan
 
-   def add_configuration_file(self, file_path: str):
-      self._soda_scan.add_configuration_yaml_file(file_path=file_path)
+   def add_ds_configuration(self, data_source: str) -> None:
+      logger.info("add_ds_configuration - soda")
+      self._soda_scan.add_configuration_yaml_str(data_source)
 
-   def add_variables(self, variables: Dict[str, str]):
-      self._soda_scan.add_variables(variables)
+   def add_variables(self, variables: Union[List[Dict[str, str]], Dict[str, str]]) -> None:
+      if isinstance(variables, list):
+         for var in variables:
+            self._soda_scan.add_variables(var)
+      elif isinstance(variables, dict):
+         self._soda_scan.add_variables(variables)
+      else:
+         logger.info("None variables were declared")
 
-   def add_sodacl_checks(self, checks_file_path: str):
-      # TODO: Improve this to recive file or files or str
-      self._soda_scan.add_sodacl_yaml_files(checks_file_path)
+   def add_sodacl_checks(self, checks_file_path: str) -> None:
+
+      if isinstance(checks_file_path, list):
+         for individual_check in checks_file_path:
+            self._soda_scan.add_sodacl_yaml_files(individual_check)
+      elif isinstance(checks_file_path, str):
+         self._soda_scan.add_sodacl_yaml_files(checks_file_path)
+      else:
+         logger.info("No check was declared")
+         raise
